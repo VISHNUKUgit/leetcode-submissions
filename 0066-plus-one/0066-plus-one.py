69e279fba1f19ad9766ed63e8a742cfd05b032fb
Vishnu KU <134578493+VISHNUKUgit@users.noreply.github.com>
2026-08-30 22:15:23 +0530

    Time: 0 ms (100%), Space: 12.2 MB (99.64%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        lastindex = len(digits) - 1
+        for i in range (lastindex,-1,-1):
+            if digits[i] != 9 :
+                digits[i] += 1 
+                return digits
+            digits[i] = 0
+        digits.insert(0,1)
+
+        return digits
