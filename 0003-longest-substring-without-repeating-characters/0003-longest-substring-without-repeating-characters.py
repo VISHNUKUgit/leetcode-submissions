ed402feabf738e739aab658deb1cd3a9f7f61dad
Vishnu KU <134578493+VISHNUKUgit@users.noreply.github.com>
2026-09-07 22:02:32 +0530

    Time: 258 ms (57.99%), Space: 13.5 MB (52.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,12 +5,12 @@ class Solution(object):
         max_length = 0
 
         for right, char in enumerate(s):
-
-            if char in char_map and char_map[char] >= left:
+            if char in char_map and char_map[char] >= left : 
                 left = char_map[char] + 1
 
             char_map[char] = right
 
-            max_length = max(max_length, right - left + 1)
+            max_length = max(max_length , right - left + 1 )
 
         return max_length
+
